7ca276958499a975404dd065f4f97adc4328ddc8
asherkuu <asher_kuu@naver.com>
2022-11-02 12:13:54 +0900

    feat: add husky post-merge

--- bash/pull.sh
+++ bash/pull.sh
@@ -5,11 +5,11 @@ echo "::: Run Pull Start :::"
 cd /var/www/crontab-test
 git pull origin develop # or other branch
 
-if ! git diff HEAD^ HEAD --exit-code -- ./package.json; then
-    echo ":::::::: Run script npm install ::::::::"
-    npm install 
-    # or todo something
-fi
+# if ! git diff HEAD^ HEAD --exit-code -- ./package.json; then
+#     echo ":::::::: Run script npm install ::::::::"
+#     npm install 
+#     # or todo something
+# fi
 
 echo "::: Run Pull End :::" #
 
